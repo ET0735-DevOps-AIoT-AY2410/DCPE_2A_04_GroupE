67b39bc12d4b6a69358d9a18a2dff25b316c21e5
Hareneraj <HARENERAJ12.23@ichat.sp.edu.sg>
2024-07-01 15:24:07 +0800

    sprint 4

---
+++ DevOps Lab Project/tempandhumidity_checker.py
@@ -0,0 +1,37 @@
+import time
+from hal import hal_lcd as lcd
+from hal import hal_temp_humidity_sensor as TEMP_HUMIDITY
+from hal import hal_keypad as KEYPAD
+
+
+
+
+def get_temperature():   #ADD THIS WHOLE FUNCTION IN MAIN MENU FOR LINE 4 !!!
+    # Read the temperature from the sensor
+    
+    tempandhumidity = TEMP_HUMIDITY.read_temp_humidity()
+    return tempandhumidity
+
+def display_temperature():  #ADD THIS WHOLE FUNCTION IN MAIN MENU FOR LINE 4 !!!
+    tempandhumidity = get_temperature()
+    lcd.clear()
+    lcd.lcd_display_string ("Temperature = %-3.1f C" % tempandhumidity.temperature, 0)
+    lcd.lcd_display_string ("Humidity: %-3.1f %%" % tempandhumidity.humidity, 1)
+    
+    time.sleep(2)
+
+def main():
+    # Initialize the LCD and the temperature/humidity sensor
+    lcd = lcd.lcd()  #init lcd
+    TEMP_HUMIDITY.init() #init sensor
+    KEYPAD.init() #initialise keypad
+    
+    global optionvalue #value of number/s keyed in
+    global option #number keyed in
+    optionvalue.append(option)
+    if optionvalue == "5":  #if option 5 is chosen to display temp and humidity
+        
+        display_temperature()
+
+    if __name__ == "__main__":
+        main()
