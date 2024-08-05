4d8d31f14185e835fb02f00b1d53ba2b262a838d
EdwardTi <edwardti2004@gmail.com>
2024-08-05 15:57:53 +0800

    -

--- src/test_startEngine.py
+++ src/test_startEngine.py
@@ -21,31 +21,13 @@ class MockMotor:
 # Function to replace RFID reader and motor in engine_starter
 def start_engine_with_mocks(card_id):
     import engine_starter
-<<<<<<< HEAD
-
-=======
     
->>>>>>> test_code_1
     # Replace RFID reader and motor with mocks
     rfid_reader = MockRFIDReader(card_id=card_id)
     motor = MockMotor()
 
     # Call the start_engine function with mocks
     engine_starter.start_engine(rfid_reader, motor)
-<<<<<<< HEAD
-
-    return motor.speeds
-
-# Tests
-def test_start_engine_multiple():
-    card_ids = [830894050716, 1234567890]  # Authorized card IDs
-    speeds = []
-    for card_id in card_ids:
-        speeds.extend(start_engine_with_mocks(card_id))
-    assert speeds == [100, 0, 100, 0]  # Motor should start and then stop for each card
-    print("Multiple test passed")
-
-=======
     
     return motor.speeds
 
@@ -54,7 +36,6 @@ def test_start_engine_authorized():
     speeds = start_engine_with_mocks(830894050716)  # Authorized card ID
     assert speeds == [100, 0]  # Motor should start and then stop
     print("Authorized test passed")
->>>>>>> test_code_1
 
 def test_start_engine_unauthorized():
     speeds = start_engine_with_mocks(9999999999)  # Unauthorized card ID
@@ -62,10 +43,6 @@ def test_start_engine_unauthorized():
     print("Unauthorized test passed")
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    test_start_engine_multiple()
-=======
     test_start_engine_authorized()
->>>>>>> test_code_1
     test_start_engine_unauthorized()
     print("All tests passed")
