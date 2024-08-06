048412fb3ac63995a8c32f975509fcf267eebf39
ezelllow <EZELLLOWQW29.23@ichat.sp.edu.sg>
2024-08-07 01:16:19 +0800

    working docker and requirments

--- src/Web_page/website/__init__.py
+++ src/Web_page/website/__init__.py
@@ -4,6 +4,7 @@ from os import path
 from flask_login import LoginManager
 import threading
 import time
+import os
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
@@ -11,7 +12,8 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
     from .views import views
